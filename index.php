bab62389f9cb1a625d3899d9edf194714d8e617c
kuninaka <abc13732@icloud.com>
2019-11-29 18:59:00 +0900

    master

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<!-- Dragon Quest Occupational diagnosis -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="./css/reset.css">
+  <link rel="stylesheet" href="./css/style.css">
+  <title>DQOD</title>
+</head>
+<body>
+  nanndeyanenn
+  <script src="./js/app.js"></script>
+</body>
+</html>
